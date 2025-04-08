0609bcbfef256911db4efac51f31601b1b14cfd8
mmac <mmac@fff.com>
2025-04-08 11:02:42 +0800

    2025-04-08 查找选择数组的最小值，二分查找

--- Array/findMin.cpp
+++ Array/findMin.cpp
@@ -42,7 +42,17 @@
 class Solution {
 public:
     int findMin(vector<int>& nums) {
+        if(nums.size()==1) return nums[0];
 
+        int left=0,right=nums.size()-1;
+        while(left< right){
+            int mid = (left + right)/2;
+            if(nums[mid] > nums[right])
+                left =  mid+1;
+            else
+                right = mid;
+        }
+        return nums[left];
     }
 };
 //leetcode submit region end(Prohibit modification and deletion)
